835a96273244a2470c359d5205d398476e747d90
David Almeida <davidalmeida0147@gmail.com>
2025-08-28 00:55:11 -0300

    "feat: adjust cors in backend"

--- Back/src/server.ts
+++ Back/src/server.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 import { AppDataSource } from './data-source';
 import routes from './routes/index';
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
+const route = process.env.ROUTE || 'http://localhost:5173/'
 
 AppDataSource.initialize().then(() => {
     const app = express();
 
     app.use(express.json());
+    app.use(cors({
+        origin: route
+    }));
+    
     app.use(routes);
     return app.listen(port, () => {
         console.log('start application');
